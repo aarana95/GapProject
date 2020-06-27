d6eca10b8d240ccf8b202559a2cccefcb8c00035
anderTheBridge <ander@thebridge.tech>
2020-06-27 10:11:50 +0200

    prueba git

--- R/nuevo.R
+++ R/nuevo.R
@@ -1,1 +1,41 @@
-generando nue
+
+
+funcionPrincipal <- function(directorioProyecto){
+  
+  tryCatch(expr = {
+    
+    
+    #Activamos el log
+    
+    
+    
+    #Leemos el archivo de configuracion
+    
+    config <- leerConfig(directorioProyecto)
+    
+    
+    #Leemos los datos
+    
+    datos <- leerDatos(config, directorioProyecto)
+    
+    
+    #Preprocesamos los datos
+    
+    
+    #Generamos el modelo
+    
+    
+    
+    #Generamos el output
+  
+  }, error = function(e){
+    
+    logerror("La aplicacion ha petado...", logger = 'log')
+    stop()
+    
+  },finally = {
+    
+    loginfo("Fin de la ejecucion.", logger = 'log')
+    
+  })
+}
